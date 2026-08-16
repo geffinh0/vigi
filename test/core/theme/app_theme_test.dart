db1a1ba4d3d25a0552761e18743877f665522467
geffinh0 <contato.gefferson@hotmail.com>
2026-08-15 23:13:56 -0300

    test(core): stabilize app_theme_test with testWidgets binding

--- test/core/theme/app_theme_test.dart
+++ test/core/theme/app_theme_test.dart
@@ -5,27 +5,12 @@ import 'package:guardiao/core/theme/app_theme.dart';
 
 void main() {
   group('AppTheme', () {
-    test('colorScheme.primary é AppColors.petroleo', () {
+    testWidgets('colorScheme e propriedades do tema', (tester) async {
       expect(appTheme.colorScheme.primary, equals(AppColors.petroleo));
-    });
-
-    test('colorScheme.secondary é AppColors.ambar', () {
       expect(appTheme.colorScheme.secondary, equals(AppColors.ambar));
-    });
-
-    test('colorScheme.error é AppColors.panico', () {
       expect(appTheme.colorScheme.error, equals(AppColors.panico));
-    });
-
-    test('scaffoldBackgroundColor é AppColors.linho', () {
       expect(appTheme.scaffoldBackgroundColor, equals(AppColors.linho));
-    });
-
-    test('brightness é light', () {
       expect(appTheme.colorScheme.brightness, equals(Brightness.light));
-    });
-
-    test('usa Material 3', () {
       expect(appTheme.useMaterial3, isTrue);
     });
   });
