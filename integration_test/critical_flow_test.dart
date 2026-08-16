9da2284b9044dc0647f87de3e3a3b732adc07e55
geffinh0 <contato.gefferson@hotmail.com>
2026-08-15 23:08:19 -0300

    chore(test): clean unused imports in integration test

--- integration_test/critical_flow_test.dart
+++ integration_test/critical_flow_test.dart
@@ -30,7 +30,6 @@ import 'package:guardiao/features/family/domain/usecases/accept_family_invite_us
 import 'package:guardiao/features/family/domain/usecases/get_family_links_usecase.dart';
 import 'package:guardiao/features/family/domain/usecases/send_family_invite_usecase.dart';
 import 'package:guardiao/features/family/presentation/bloc/family_bloc.dart';
-import 'package:guardiao/features/panic/domain/entities/panic_alert_entity.dart';
 import 'package:guardiao/features/panic/domain/repositories/panic_repository.dart';
 import 'package:guardiao/features/panic/domain/usecases/resolve_panic_usecase.dart';
 import 'package:guardiao/features/panic/domain/usecases/trigger_panic_usecase.dart';
@@ -39,84 +38,102 @@ import 'package:guardiao/injection/injection_container.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthRepository extends Mock implements AuthRepository {}
+
 class MockCheckinRepository extends Mock implements CheckinRepository {}
+
 class MockContactsRepository extends Mock implements ContactsRepository {}
+
 class MockPanicRepository extends Mock implements PanicRepository {}
+
 class MockFamilyRepository extends Mock implements FamilyRepository {}
 
 void main() {
   group('Critical Flow Test (Foreground E2E)', () {
-    testWidgets('Fluxo crítico: inicialização -> tela inicial -> mascote e anel ativos', (tester) async {
-      if (!sl.isRegistered<Clock>()) sl.registerSingleton<Clock>(const SystemClock());
-      if (!sl.isRegistered<Ticker>()) sl.registerSingleton<Ticker>(const Ticker());
+    testWidgets(
+      'Fluxo crítico: inicialização -> tela inicial -> mascote e anel ativos',
+      (tester) async {
+        if (!sl.isRegistered<Clock>()) {
+          sl.registerSingleton<Clock>(const SystemClock());
+        }
+        if (!sl.isRegistered<Ticker>()) {
+          sl.registerSingleton<Ticker>(const Ticker());
+        }
 
-      final authRepo = MockAuthRepository();
-      when(() => authRepo.authStateChanges).thenAnswer((_) => const Stream.empty());
+        final authRepo = MockAuthRepository();
+        when(
+          () => authRepo.authStateChanges,
+        ).thenAnswer((_) => const Stream.empty());
 
-      final contactsRepo = MockContactsRepository();
-      final checkinRepo = MockCheckinRepository();
-      when(() => checkinRepo.getStatus()).thenAnswer(
-        (_) async => const Right(MonitoringStatusEntity(active: false, intervalMinutes: 60)),
-      );
-
-      final panicRepo = MockPanicRepository();
-      final familyRepo = MockFamilyRepository();
-
-      final authBloc = AuthBloc(
-        signInUseCase: SignInUseCase(authRepo),
-        signUpUseCase: SignUpUseCase(authRepo),
-        signOutUseCase: SignOutUseCase(authRepo),
-        authRepository: authRepo,
-      );
-
-      final checkinBloc = CheckinBloc(
-        startMonitoringUseCase: StartMonitoringUseCase(checkinRepo, contactsRepo),
-        confirmCheckinUseCase: ConfirmCheckinUseCase(checkinRepo),
-        stopMonitoringUseCase: StopMonitoringUseCase(checkinRepo),
-        getMonitoringStatusUseCase: GetMonitoringStatusUseCase(checkinRepo),
-        ticker: sl(),
-        clock: sl(),
-      );
-
-      final contactsBloc = ContactsBloc(
-        getContactsUseCase: GetContactsUseCase(contactsRepo),
-        addContactUseCase: AddContactUseCase(contactsRepo),
-        deleteContactUseCase: DeleteContactUseCase(contactsRepo),
-      );
-
-      final panicBloc = PanicBloc(
-        triggerPanicUseCase: TriggerPanicUseCase(panicRepo),
-        resolvePanicUseCase: ResolvePanicUseCase(panicRepo),
-      );
-
-      final familyBloc = FamilyBloc(
-        getFamilyLinksUseCase: GetFamilyLinksUseCase(familyRepo),
-        sendFamilyInviteUseCase: SendFamilyInviteUseCase(familyRepo),
-        acceptFamilyInviteUseCase: AcceptFamilyInviteUseCase(familyRepo),
-      );
-
-      await tester.pumpWidget(
-        MultiBlocProvider(
-          providers: [
-            BlocProvider<AuthBloc>.value(value: authBloc),
-            BlocProvider<CheckinBloc>.value(value: checkinBloc),
-            BlocProvider<ContactsBloc>.value(value: contactsBloc),
-            BlocProvider<PanicBloc>.value(value: panicBloc),
-            BlocProvider<FamilyBloc>.value(value: familyBloc),
-          ],
-          child: MaterialApp(
-            theme: appTheme,
-            home: const GuardiaoHomePage(),
+        final contactsRepo = MockContactsRepository();
+        final checkinRepo = MockCheckinRepository();
+        when(() => checkinRepo.getStatus()).thenAnswer(
+          (_) async => const Right(
+            MonitoringStatusEntity(active: false, intervalMinutes: 60),
           ),
-        ),
-      );
-      await tester.pump();
+        );
 
-      // Verifica presença dos elementos visuais da Home
-      expect(find.text('Guardião'), findsOneWidget);
-      expect(find.byType(VigiMascot), findsOneWidget);
-      expect(find.byType(StatusRing), findsOneWidget);
-      expect(find.text('Você está protegido pelo Vigi'), findsOneWidget);
-    });
+        final panicRepo = MockPanicRepository();
+        final familyRepo = MockFamilyRepository();
+
+        final authBloc = AuthBloc(
+          signInUseCase: SignInUseCase(authRepo),
+          signUpUseCase: SignUpUseCase(authRepo),
+          signOutUseCase: SignOutUseCase(authRepo),
+          authRepository: authRepo,
+        );
+
+        final checkinBloc = CheckinBloc(
+          startMonitoringUseCase: StartMonitoringUseCase(
+            checkinRepo,
+            contactsRepo,
+          ),
+          confirmCheckinUseCase: ConfirmCheckinUseCase(checkinRepo),
+          stopMonitoringUseCase: StopMonitoringUseCase(checkinRepo),
+          getMonitoringStatusUseCase: GetMonitoringStatusUseCase(checkinRepo),
+          ticker: sl(),
+          clock: sl(),
+        );
+
+        final contactsBloc = ContactsBloc(
+          getContactsUseCase: GetContactsUseCase(contactsRepo),
+          addContactUseCase: AddContactUseCase(contactsRepo),
+          deleteContactUseCase: DeleteContactUseCase(contactsRepo),
+        );
+
+        final panicBloc = PanicBloc(
+          triggerPanicUseCase: TriggerPanicUseCase(panicRepo),
+          resolvePanicUseCase: ResolvePanicUseCase(panicRepo),
+        );
+
+        final familyBloc = FamilyBloc(
+          getFamilyLinksUseCase: GetFamilyLinksUseCase(familyRepo),
+          sendFamilyInviteUseCase: SendFamilyInviteUseCase(familyRepo),
+          acceptFamilyInviteUseCase: AcceptFamilyInviteUseCase(familyRepo),
+        );
+
+        await tester.pumpWidget(
+          MultiBlocProvider(
+            providers: [
+              BlocProvider<AuthBloc>.value(value: authBloc),
+              BlocProvider<CheckinBloc>.value(value: checkinBloc),
+              BlocProvider<ContactsBloc>.value(value: contactsBloc),
+              BlocProvider<PanicBloc>.value(value: panicBloc),
+              BlocProvider<FamilyBloc>.value(value: familyBloc),
+            ],
+            child: MaterialApp(
+              theme: appTheme,
+              home: const GuardiaoHomePage(),
+            ),
+          ),
+        );
+        await tester.pump();
+
+        // Verifica presença dos elementos visuais da Home
+        expect(find.text('Guardião'), findsOneWidget);
+        expect(find.byType(VigiMascot), findsOneWidget);
+        expect(find.byType(StatusRing), findsOneWidget);
+        expect(find.text('Você está protegido pelo Vigi'), findsOneWidget);
+      },
+    );
   });
 }
